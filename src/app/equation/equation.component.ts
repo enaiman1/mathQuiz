c1caab58b8ae09a6fc8e5353978cc4828cbb5095
Ericcentrik <enaiman1@gmail.com>
2020-03-26 22:33:50 -0400

    passed value for both numbers and answer

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl} from '@angular/forms'
 
 @Component({
   selector: 'app-equation',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equation.component.css']
 })
 export class EquationComponent implements OnInit {
-
+mathForm = new FormGroup({
+  num1: new FormControl(this.randomNumber()),
+  num2: new FormControl(this.randomNumber()),
+  answer: new FormControl(""),
+})
   constructor() { }
 
+get num1(){
+  return this.mathForm.value.num1
+}
+get num2(){
+  return this.mathForm.value.num2
+}
+
   ngOnInit() {
+  }
+
+  randomNumber(){
+    return Math.floor(Math.random() * 10)
   }
 
 }
